89a850f609c4f083dcc73db2312d5bd1a7a738e8
Benjamin <hoppeconnect@gmail.com>
2018-11-30 09:59:54 -0500

    Completed part 3

--- crm.rb
+++ crm.rb
@@ -6,13 +6,25 @@ get '/home' do
 end
 
 get '/contacts' do
-  @contacts = Contact.all
+  @contact = Contact.all
   erb :contacts
 end
 
 get '/about' do
   erb :about
 end
+
+
+get '/contacts/:id' do
+  @contact = Contact.find_by({id: params[:id].to_i})
+  erb :show_contact
+  if @contact
+   erb :show_contact
+ else
+   raise Sinatra::NotFound
+ end
+end
+
 
 after do
   ActiveRecord::Base.connection.close
