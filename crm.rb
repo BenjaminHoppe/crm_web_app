97a721dbef4210e434142efed475820677c07545
Benjamin <hoppeconnect@gmail.com>
2018-11-30 19:21:03 -0500

    finished crm web app part 4

--- crm.rb
+++ crm.rb
@@ -14,6 +14,9 @@ get '/about' do
   erb :about
 end
 
+get '/contacts/new' do
+  erb :new
+end
 
 get '/contacts/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
@@ -23,8 +26,54 @@ get '/contacts/:id' do
  else
    raise Sinatra::NotFound
  end
+  erb :show_contact
 end
 
+post '/contacts' do
+  Contact.create(
+  first_name: params[:first_name],
+  last_name:  params[:last_name],
+  email:      params[:email],
+  note:       params[:note]
+)
+  redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
